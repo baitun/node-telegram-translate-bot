d6914e1455e9518a67e7442af75570b197a6608c
savinyurii <git@savinyurii.ru>
2018-08-12 15:46:32 +0800

    forward messages instead of createing new one

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name      : 'bot',
+    name      : 'translate_bot',
     script    : 'index.js',
     env: {
       NODE_ENV: 'development'
@@ -8,5 +8,16 @@ module.exports = {
     env_production : {
       NODE_ENV: 'production'
     }
-  }]
+  }],
+
+  deploy : {
+    production : {
+      user : 'savin',
+      host : 'node3.ortant.ru',
+      ref  : 'origin/master',
+      repo : 'git@bitbucket.org:savinyurii/translate_bot.git',
+      path : '/home/projects/savin/translate_bot/',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 };
